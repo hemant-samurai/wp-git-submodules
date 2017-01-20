b24e7330e6630016e2979059e39f04983e8294a1
Hemant <hemant.acharya@samurai.company>
2017-01-20 20:46:02 +0000

    Initial commit

---
+++ local-config.php
@@ -0,0 +1,18 @@
+<?php
+define( 'DB_NAME', 'wordpress' );
+define( 'DB_USER', 'exampleuser' );
+define( 'DB_PASSWORD', 'examplepassword' );
+define( 'DB_HOST', 'localhost' );
+
+ini_set( 'display_errors', E_ALL );
+define( 'WP_DEBUG_DISPLAY', true );
+define( 'WP_DEBUG', true );
+
+define('AUTH_KEY',         'put your unique phrase here');
+define('SECURE_AUTH_KEY',  'put your unique phrase here');
+define('LOGGED_IN_KEY',    'put your unique phrase here');
+define('NONCE_KEY',        'put your unique phrase here');
+define('AUTH_SALT',        'put your unique phrase here');
+define('SECURE_AUTH_SALT', 'put your unique phrase here');
+define('LOGGED_IN_SALT',   'put your unique phrase here');
+define('NONCE_SALT',	   'put your unique phrase here');
